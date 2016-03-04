5b730405a4eb7beb6b3502ba22af1b4ca950c920
ProkopenkoRoman <long399@mail.ru>
2016-03-04 22:33:52 +0600

    Update LabW1.c

--- LabW1.c
+++ LabW1.c
@@ -1,70 +1,73 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <malloc.h>
 
 void sspn(char *s);
 int slen(char *s);
-int stok(char *str, char delim, char *ptr[]);	
-int schr(char *str, char delim);
-int sequal(char *s, char *s2);
-void scopy(char *s, char *s2);
-void suntok(char *str, char delim, char *ptr[], int cnt);
+int stok(char *s, char delim, char *ptr[]);	
+int schr(char *s, char delim);
+int sequal(char *s1, char *s2);
+void scopy(char *s1, char *s2);
+void suntok(char *s, char delim, char *ptr[], int cnt);
 
-int main()
-{
-	int i,j,k,f,t;
-	char dir1[] = "~/";
-	char dir2[] = "o/";
-	char del;
-	char str[] = "~/games/packman.cpp+~al*x/docs+~/study/Prog/lab4.c+/u*er/bin/gcc";
+int main() {
+	int i, j, k;
+	//char ch[260]; // vremenniy massiv dlya dannih
+	char delim; // razdelitel'
 	char *ptr[10];
-	printf("delim: ");
-	scanf("%c",&del);
 	
-/*	printf("paths: ");
-	scanf("%c\n",&str);
+	char somedir[] = "~/";
+	char username[] = "John";
+	char dir[] = "o/";
+	char paths[] = "~/games/packman.cpp+~alex/d*cs+~/study/Prog/lab4.c+/us*r/bin/gcc+~/lab/pr*ject1";
 	
-	printf("dir1: ");
-	scanf("%c\n",&dir1);
-	printf("dir2: ");
-	scanf("%c\n",&dir2);*/
-	
-	i = stok(str, del, ptr);
+    printf("Vvedite razdlitel':\n");
+      scanf("%c",&delim); // razdelitel'
+ /*   printf("Vvedite imya polzovatelya:\n");
+      scanf("%s", &ch);
+       char *username = (int *)malloc(slen(ch)*sizeof(int)); // imya polzovatelya
+        for (i=0; i<slen(ch); i++)
+         username[i]=ch[i];
+    printf("Vvedite imya domashney direktorii:\n");
+      scanf("%s", &ch);
+       char *dir = (int *)malloc(slen(ch)*sizeof(int)); // imya domashnego kataloga
+        for (i=0; i<slen(ch); i++)
+         dir[i]=ch[i];
+    printf("Vvedite puti cherez razdeliteli:\n");
+      scanf("%s", &ch);
+       char *paths = (int *)malloc(slen(ch)*sizeof(int)); // vhodnaya stroka s putyami
+        for (i=0; i<slen(ch); i++)
+         paths[i]=ch[i]; */
+    
+	i = stok(paths, delim, ptr);
 	for(j = 0;  j < i; j++) {
 		sspn(ptr[j]);
 	}
+	
 	for(j = 0;  j < i; j++) {
-		f = sequal(ptr[j], dir1);
-		if(f == 1){
-			scopy(dir2, ptr[j]);
+		k = sequal(ptr[j], somedir);
+		if(k == 1){
+			scopy(dir, ptr[j]);
 		}
 	}
 	
-	suntok(str, del, ptr, i);
-	printf("%s",str);
+	suntok(paths, delim, ptr, i);
+	printf("New paths: %s\n", paths);
 	
-	return 0;
+    return 0;
 }
 
-/*копирование строки*/
-void scopy(char *s, char *s2)
-{
-	int i;
-	for(i = 0; (s[i] != '\0' && s2[i] != '\0'); i++)
-		s2[i] = s[i];
-	
-}
-/*размер строки*/
-int slen(char *s)
-{
+// dlina stroki
+int slen(char *s) {
     int i;
     for (i = 0; s[i] != '\0'; i++);
     return i;
 }
-/*разделение на стоки*/
-int stok(char *str, char delim, char *ptr[])
-{
-	char *suf = str;
-	ptr[0] = str; 
+
+// razbienie stroki na podstroki
+int stok(char *s, char delim, char *ptr[]) {
+	char *suf = s;
+	ptr[0] = s; 
 	int i, j = 1; 
 	while( ( i = schr(suf, delim) ) >= 0 ){
 		suf[i] = '\0';
@@ -72,45 +75,21 @@ int stok(char *str, char delim, char *ptr[])
 		ptr[j] = suf;
 		j++;
 	}
-	
 	return j;
 }
-/*собирание строки*/
-void suntok(char *str, char delim, char *ptr[], int cnt)
-{
-	int i;
-	for(i = 1; i < cnt; i++){
-		*(ptr[i] - 1) = delim;
-	}
-}
-/*поиск символа в строке*/
-int schr(char *str, char delim)
-{
+
+// poisk simvola v stroke
+int schr(char *s, char delim) {
 	int i, idx = -1;
-	for(i = 0; (str[i] != '\0') && (str[i] != delim); i++);
-		if(str[i] == delim)
+	for(i = 0; (s[i] != '\0') && (s[i] != delim); i++);
+		if(s[i] == delim)
 			idx = i;
 	return idx;
 }
-/*сравнение строк*/ 
-int sequal(char *s, char *s2)
-{
-	int i, out = 1;
-	for(i = 0; out && (s[i] != '\0' && s2[i] != '\0'); i++){
-		if (s[i] != s2[i]){
-			out = 0;
-			return out;
-		}
-	}
-	//out = 1;
-	return out;
-}
-/*проверка на принадлежность символа данному множеству*/
 
-void sspn(char *s)
-{
+// proverka stroki na nedopustimie simvoli
+void sspn(char *s) {
 	int i;
-	
 	for(i = 0;  s[i] != '\0'; i++){
 		if (s[i] ==  '*' ){
 			
@@ -118,5 +97,34 @@ void sspn(char *s)
 			printf("%s\n",s);
 			//exit(EXIT_FAILURE);
 		}
+	}
+}
+
+// sravnenie strok
+int sequal(char *s1, char *s2) {
+	int i, out = 1;
+	for(i = 0; out && (s1[i] != '\0' && s2[i] != '\0'); i++){
+		if (s1[i] != s2[i]){
+			out = 0;
+			return out;
+		}
+	}
+	//out = 1;
+	return out;
+}
+
+// kopirovanie stroki
+void scopy(char *s1, char *s2) {
+	int i;
+	for(i = 0; (s1[i] != '\0' && s2[i] != '\0'); i++)
+		s2[i] = s1[i];
+	
+}
+
+// sobiranie stroki
+void suntok(char *s, char delim, char *ptr[], int cnt) {
+	int i;
+	for(i = 1; i < cnt; i++){
+		*(ptr[i] - 1) = delim;
 	}
 }
