cce9809b1504559f4738f5ba4c0b7fc03cc76d8b
ProkopenkoRoman <long399@mail.ru>
2016-03-26 21:05:03 +0600

    Update lab1lvl.c
    Добавлены англ. комментарии

--- lab1lvl.c
+++ lab1lvl.c
@@ -24,14 +24,14 @@ int main() {
 	char str[261] = "/home/qwert/9/openmpi/165/lib/OpenMPI/mca_btl_tcp.so"; // linux
 	//char *suf = str;
 
-	//printf("Ââåäèòå ñòðîêó, ñîîòâåòñòâóþùàóþ àáñîëþòíîìó ïóòè:\n");
+	//printf("Ââåäèòå ñòðîêó, ñîîòâåòñòâóþùóþ àáñîëþòíîìó ïóòè:\n"); // vvedite stroku, sootvetstvuyushuyu absolutnomu puti
 	//scanf("%s\n", &str);
 	
-	printf("Âõîäíàÿ ñòðîêà: %s\n\n", str);
-	sspn(str); // ïðîâåðêà íà íåäîïóñòèìûå ñèìâîëû
+	printf("Âõîäíàÿ ñòðîêà: %s\n", str); // vhodnaya stroka
+	sspn(str); // proverka na nedopustimie simvoli
 	printf("\n");
 	
-	////////////////////////////////////// ÎÏÐÅÄÅËÅÍÈÅ OS //////////////////////////////////////
+	////////////////////////////////////// OPREDELENIE OS //////////////////////////////////////
 	first_ch = str[0];
 	if (first_ch == '/') {
 		printf("OS: Linux\n");
@@ -44,40 +44,39 @@ int main() {
 		f=1;
 	}
 	if (f == 0) {
-		printf("Îøèáî÷íûé ïóòü...\n");
+		printf("Îøèáî÷íûé ïóòü...\n"); // oshibo4niy put'
 		return 1;
 	}
 	
-	printf("Ðàçäåëèòåëü: %c\n", delim);
+	printf("Ðàçäåëèòåëü: %c\n", delim); // razdelitel'
       	
 	i = stok(str, delim, ptr);
-	printf("Êîëè÷åñòâî êàòàëîãîâ ñ ó÷åòîì êîíå÷íîãî ôàéëà: %d\n", i);
+	printf("Êîëè÷åñòâî êàòàëîãîâ ñ ó÷åòîì êîíå÷íîãî ôàéëà: %d\n", i); // koli4estvo katalogov s u4etom kone4nogo fayla
 	
-	printf("Êîíå÷íûé ôàéë: %s\n", ptr[i-1]);
+	printf("Êîíå÷íûé ôàéë: %s\n", ptr[i-1]); // kone4niy fayl
 	
 	s = slen(ptr[i-1]);
-	printf("Äëèíà êîíå÷íîãî ôàéëà: %d\n\n", s);
+	printf("Äëèíà êîíå÷íîãî ôàéëà: %d\n\n", s); // dlina kone4nogo fayla
 	
 	for(k = 0; k < i; k++) {
 		if (atoi(ptr[k])) {
 			j = atoi(ptr[k])+1;
-			printf("×èñëîâîé êàòàëîã, êîòîðûé íóæíî óâåëè÷èòü íà 1: <%d êàòàëîã> = %d\n", k+1, j);
+			printf("×èñëîâîé êàòàëîã, êîòîðûé íóæíî óâåëè÷èòü íà 1: <%d êàòàëîã> = %d\n", k+1, j); // 4islovoy katalog, kotoriy nuzhno uveli4it' na 1
 			if (((j - 1) % 1000 == 999 || (j - 1) % 1000 == 99 || (j - 1) % 1000 == 9) && ((j - 1) % 100 == 99 || (j - 1) % 100 == 9) && ((j - 1) % 10 == 9)) {
-				printf("Íóæíî ñâäèíóòü ñòðîêó íà 1 âïðàâî, íà÷èíàÿ ñ %d êàòàëîãà\n", k+1);
-				//sdvig(str, k + 1, i, delim, ptr);
+				printf("Íóæíî ñâäèíóòü ñòðîêó íà 1 âïðàâî, íà÷èíàÿ ñ %d êàòàëîãà\n", k+1); // nuzhno sdvinut' stroku na 1 vpravo, na4inaya s kataloga
+			//	sdvig(str, k + 1, i, delim, ptr);
 			}
 			change(ptr[k], j);
 		}
 	}
 	
 	suntok(str, delim, ptr, i);
-	printf("\nÎáíîâëåííàÿ ñòðîêà: %s\n", str);
+	printf("\nÎáíîâëåííàÿ ñòðîêà: %s\n", str); // obnovlennaya stroka
 	
     return 0;
 }
 
-
-// ñäâèã ñòðîêè
+// sdvig stroki i ukazateley
 void sdvig(char *s, int j, int i, char delim, char *ptr[]) {
 	int m, count = 0;
 	for (m = slen(s); count < i - j; m--) {
@@ -91,13 +90,12 @@ void sdvig(char *s, int j, int i, char delim, char *ptr[]) {
 	}
 }
 
-
-// çàìåíà ñòðîêè
+// zamena kataloga <n> na <n+1>
 void change(char *s, int k) {
 char* c;
     c = (char *)malloc(10 * sizeof(char)); 
-    int i, v = 0; //êîëè÷åñòâî öèôð â ÷èñëå n
-    //ðàçáèâàåì íà îòäåëüíûå ñèìâîëû ÷èñëî n
+    int i, v = 0; // koli4estvo cifr v 4isle n
+    // razbiaem na otdel'nie simvoli 4islo n
     while (k > 9)
     {
         c[v++] = (k % 10) + '0';
@@ -106,7 +104,7 @@ char* c;
     c[v++] = k + '0';
     c[v] = '\0';
     char t;
-    //èíâåðòèðóåì ìàññèâ ñèìâîëîâ
+    // invertiruem massiv simvolov
     for (i = 0; i < v / 2; i++)
     {
         t = c[i];
@@ -119,32 +117,28 @@ char* c;
     free(c);
 }
 
-
-// ïðèíàäëåæíîñòü ê ëàòèíñêèì áóêâàì íèæíåãî ðåãèñòðà
+// prinadlezhnost' k latinskim bukvam nizhnego registra
 int isLowCase(char ch) {
 if( ch >= 'a' && ch <= 'z')
 return 1;
 return 0;
 }
 
-
-// ïðèíàäëåæíîñòü ê ëàòèíñêèì áóêâàì âåðõíåãî ðåãèñòðà
+// prinadlezhnost' k latinskim bukvam verhnego registra
 int isHighCase(char ch) {
 if( ch >= 'A' && ch <= 'Z')
 return 1;
 return 0;
 }
 
-
-// äëèíà ñòðîêè
+// dlina stroki
 int slen(char *s) {
     int i;
     for (i = 0; s[i] != '\0'; i++);
     return i;
 }
 
-
-// ðàçáèåíèå ñòðîêè íà ïîäñòðîêè
+// razbienie stroki na podstroki
 int stok(char *s, char delim, char *ptr[]) {
 	char *suf = s;
 	ptr[0] = s; 
@@ -158,8 +152,7 @@ int stok(char *s, char delim, char *ptr[]) {
 	return j;
 }
 
-
-// ïîèñê ñèìâîëà â ñòðîêå
+// poisk simvola v stroke
 int schr(char *s, char delim) {
 	int i, idx = -1;
 	for(i = 0; (s[i] != '\0') && (s[i] != delim); i++);
@@ -168,18 +161,16 @@ int schr(char *s, char delim) {
 	return idx;
 }
 
-
-// ïðîâåðêà ñòðîêè íà íåäîïóñòèìûå ñèìâîëû
+// proverka stroki na nedopustimie simvoli
 void sspn(char *s) {
 	int i;
 	for(i = 0;  s[i] != '\0'; i++){
 		if (s[i] ==  '*' || s[i] ==  '&' || s[i] ==  '?' || s[i] ==  '|' || s[i] ==  '"' || s[i] ==  '<' || s[i] ==  '>' || s[i] ==  ';')
-			fprintf(stderr, "Íåäîïóñòèìûé ñèìâîë <%c> íà ïîçèöèè <%d>;\n", s[i], i);
+			fprintf(stderr, "Íåäîïóñòèìûé ñèìâîë <%c> íà ïîçèöèè <%d>;\n", s[i], i); // nedopustimiy simvol na pozicii
 	}
 }
 
-
-// ñðàâíåíèå ñòðîê
+// sravnenie strok
 int sequal(char *s1, char *s2) {
 	int i, out = 1;
 	for(i = 0; out && (s1[i] != '\0' && s2[i] != '\0'); i++){
@@ -188,12 +179,10 @@ int sequal(char *s1, char *s2) {
 			return out;
 		}
 	}
-	//out = 1;
 	return out;
 }
 
-
-// êîïèðîâàíèå ñòðîêè
+// kopirovanie stroki
 void scopy(char *s1, char *s2) {
 	int i;
 	for(i = 0; (s1[i] != '\0' && s2[i] != '\0'); i++)
@@ -201,8 +190,7 @@ void scopy(char *s1, char *s2) {
 	
 }
 
-
-// ñîáèðàíèå ñòðîêè
+// sobiranie stroki
 void suntok(char *s, char delim, char *ptr[], int cnt) {
 	int i;
 	for(i = 1; i < cnt; i++){
