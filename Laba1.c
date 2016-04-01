6a011521e656b219e6d13bc16a6a2f177e2bff37
ProkopenkoRoman <long399@mail.ru>
2016-04-01 21:34:42 +0600

    Update Laba1.c
    New

--- Laba1.c
+++ Laba1.c
@@ -10,58 +10,58 @@ int sequal(char *s1, char *s2);
 void scopy(char *s1, char *s2);
 void suntok(char *s, char delim, char *ptr[], int cnt);
 
-int main() 
-{
-	int i, j, k, r, s1, index, delta, n;
-	char delim; /////////// razdelitel'
+int main() {
+	int i, j, k, r, index, delta, n;
+	char delim; // razdelitel'
 	char *ptr[10];
 	char tmp;
 	
 	char somedir[] = "~/";
 	char username[] = "John";
 	char dir[] = "/home/stud/";
-	char paths[260] = "~/games/packman.cpp+~alex/docs+~/study/Prog/lab4.c+/usr/bin/gcc";
-	int size = slen(paths);
+	char paths[260] = "~/games/packman.cpp+~alex/d*cs+~/study/Prog/lab4.c+/us*r/bin/gcc";
 	char *suf = paths;
-	
 	delta = slen(somedir)-slen(dir);
-	s1 = slen(paths);
-	printf("Input razdl: ");
-	scanf("%c",&delim); /////////// razdel	
+	int size = slen(paths);
+	
+    printf("Vvedite razdlitel':\n");
+      scanf("%c",&delim); // razdelitel'
+      	
 	i = stok(paths, delim, ptr);
-	for(j = 0;  j < i; j++)
+	for(j = 0;  j < i; j++) {
 		sspn(ptr[j]);
-		
-	for(j = 0;  j < i; j++) 
+	}
+	
+	for(j = 0;  j < i; j++)
 		if(sequal(ptr[j], somedir) == 1){
 			index = ptr[j] - suf;
+			
 			if (delta < 0) {
-				for (n = s1 - delta; n > index + delta ; n--) 				 		
+				for (n = size - delta; n > index + delta ; n--)
 					paths[n - delta] = paths[n];
 				for (n = j; n < i - 1; n++)
 					ptr[n + 1] = ptr[n + 1] - delta;
 			}
-			scopy(dir, ptr[j]);
+		scopy(dir, ptr[j]);	
 		}		
+		
 	suntok(paths, delim, ptr, i);
-	printf("New paths:");
-	for (i = 0; i <= size; i++)
-		printf("%c", paths[i]);
-	printf("\n");
-	return 0;
+
+		for (i = 0; i <= size+17; i++)
+			printf("%c", paths[i]);
+	    printf("\n");
+    return 0;
 }
 
-/////////// dlina stroki
-int slen(char *s) 
-{
-	int i;
-	for (i = 0; s[i] != '\0'; i++);
-	return i;
+// dlina stroki
+int slen(char *s) {
+    int i;
+    for (i = 0; s[i] != '\0'; i++);
+    return i;
 }
 
-/////////// razbienie stroki na podstroki
-int stok(char *s, char delim, char *ptr[]) 
-{
+// razbienie stroki na podstroki
+int stok(char *s, char delim, char *ptr[]) {
 	char *suf = s;
 	ptr[0] = s; 
 	int i, j = 1; 
@@ -74,9 +74,8 @@ int stok(char *s, char delim, char *ptr[])
 	return j;
 }
 
-/////////// poisk simvola v stroke
-int schr(char *s, char delim) 
-{
+// poisk simvola v stroke
+int schr(char *s, char delim) {
 	int i, idx = -1;
 	for(i = 0; (s[i] != '\0') && (s[i] != delim); i++);
 		if(s[i] == delim)
@@ -84,21 +83,21 @@ int schr(char *s, char delim)
 	return idx;
 }
 
-/////////// proverka stroki na nedopustimie simvoli
-void sspn(char *s) 
-{
+// proverka stroki na nedopustimie simvoli
+void sspn(char *s) {
 	int i;
 	for(i = 0;  s[i] != '\0'; i++){
 		if (s[i] ==  '*' ){
+			
 			fprintf(stderr, "invalid character  %d \n", i);
 			printf("%s\n",s);
+			//exit(EXIT_FAILURE);
 		}
 	}
 }
 
-/////////// sravnenie strok
-int sequal(char *s1, char *s2) 
-{
+// sravnenie strok
+int sequal(char *s1, char *s2) {
 	int i, out = 1;
 	for(i = 0; out && (s1[i] != '\0' && s2[i] != '\0'); i++){
 		if (s1[i] != s2[i]){
@@ -106,20 +105,20 @@ int sequal(char *s1, char *s2)
 			return out;
 		}
 	}
+	//out = 1;
 	return out;
-}	
+}
 
-/////////// kopirovanie stroki
-void scopy(char *s1, char *s2) 
-{
+// kopirovanie stroki
+void scopy(char *s1, char *s2) {
 	int i;
-	for(i = 0;(s1[i] != '\0' && s2[i] != '\0'); i++)
+	for(i = 0; (s1[i] != '\0' && s2[i] != '\0'); i++)
 		s2[i] = s1[i];
 	
 }
 
-/////////// sobiranie stroki
-void suntok(char *s, char delim, char *ptr[], int cnt) {	
+// sobiranie stroki
+void suntok(char *s, char delim, char *ptr[], int cnt) {
 	int i;
 	for(i = 1; i < cnt; i++){
 		*(ptr[i] - 1) = delim;
