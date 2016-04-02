d02a2ab7e943b28d843d813f4533aa1d0ea785a1
ProkopenkoRoman <long399@mail.ru>
2016-04-02 18:36:35 +0600

    Update proc.c
    Лучшее определение ОС.

--- lab1NEW/proc.c
+++ lab1NEW/proc.c
@@ -7,16 +7,16 @@ void input(char *s) {
 	int i;
 	char ch[500];
 	
-	printf("Введите строку, соответствующую абсолютному пути:\n"); // vvedite stroku, sootvetstvuyushuyu absolutnomu puti	
+	printf("Ââåäèòå ñòðîêó, ñîîòâåòñòâóþùóþ àáñîëþòíîìó ïóòè:\n"); // vvedite stroku, sootvetstvuyushuyu absolutnomu puti	
 		scanf("%s", &ch);
 	if(slen(ch) > 261) {
-	 	printf("Превышение допустимой длины пути..."); // previshenie dopustimoy dlini puti
+	 	printf("Ïðåâûøåíèå äîïóñòèìîé äëèíû ïóòè..."); // previshenie dopustimoy dlini puti
 	 	exit(1);
 	}
 	else {
 		for (i = 0; i < slen(ch); i++)
          	s[i] = ch[i]; 
-		printf("Входная строка: %s\n\n", s); // vhodnaya stroka
+		printf("Âõîäíàÿ ñòðîêà: %s\n\n", s); // vhodnaya stroka
 	} 
 }
 
@@ -30,47 +30,47 @@ void process(char *s) {
 	
 	////////////////////////////////////// OPREDELENIE OS //////////////////////////////////////
 	first_ch = s[0];
-	if (first_ch == '/') {
+	if (first_ch == '/' && schr(s, '\\') < 0) {
 		printf("OS: Linux\n");
 		delim = '/';
 		f=1;
 	}
-	if (isLowCase(first_ch) == 1 || isHighCase(first_ch) == 1) {
+	if ((isLowCase(first_ch) == 1 || isHighCase(first_ch) == 1) && schr(s, '/') < 0 && schr(s, ':') > 0 && schr(s, ':') < 3 && schr(s, '\\') > schr(s, ':')) {
 		printf("OS: Windows\n");
 		delim = '\\';
 		f=1;
 	}
 	if (f == 0) {
-		printf("Ошибочный путь...\n"); // oshibo4niy put'
+		printf("Îøèáî÷íûé ïóòü...\n"); // oshibo4niy put'
 		exit(1);
 	}
 	
-	printf("Разделитель: %c\n", delim); // razdelitel'
+	printf("Ðàçäåëèòåëü: %c\n", delim); // razdelitel'
       	
 	i = stok(s, delim, ptr);
-	printf("Количество каталогов: %d\n", i - 2); // koli4estvo katalogov
+	printf("Êîëè÷åñòâî êàòàëîãîâ: %d\n", i - 2); // koli4estvo katalogov
 	
-	printf("Конечный файл: %s\n", ptr[i-1]); // kone4niy fayl
+	printf("Êîíå÷íûé ôàéë: %s\n", ptr[i-1]); // kone4niy fayl
 	
 	s1 = slen(ptr[i-1]);
-	printf("Длина конечного файла: %d\n\n", s1); // dlina kone4nogo fayla
+	printf("Äëèíà êîíå÷íîãî ôàéëà: %d\n\n", s1); // dlina kone4nogo fayla
 						
 	for (k = 0; k < i; k++) {
 		if (isCase(ptr[k]) == 0) {
 		if (atoi(ptr[k])) {
 			j = atoi(ptr[k])+1;
-			printf("Числовой каталог, который нужно увеличить на 1: <%d каталог> = %d\n", k, j); // 4islovoy katalog, kotoriy nuzhno uveli4it' na 1
+			printf("×èñëîâîé êàòàëîã, êîòîðûé íóæíî óâåëè÷èòü íà 1: <%d êàòàëîã> = %d\n", k, j); // 4islovoy katalog, kotoriy nuzhno uveli4it' na 1
 			if (((j - 1) % 1000 == 999 || (j - 1) % 1000 == 99 || (j - 1) % 1000 == 9) && ((j - 1) % 100 == 99 || (j - 1) % 100 == 9) && ((j - 1) % 10 == 9)) {
-				printf("Нужно свдинуть строку на 1 вправо, начиная с %d каталога\n", k); // nuzhno sdvinut' stroku na 1 vpravo, na4inaya s kataloga
+				printf("Íóæíî ñâäèíóòü ñòðîêó íà 1 âïðàâî, íà÷èíàÿ ñ %d êàòàëîãà\n", k); // nuzhno sdvinut' stroku na 1 vpravo, na4inaya s kataloga
 				sdvig(s, k + 1, i, delim, ptr);
 			}
 		change(ptr[k], j);
 		}
 		}
 	}
-	suntok(str, delim, ptr, i);	
+	suntok(s, delim, ptr, i);	
 }
 
 void output(char *s) {
-	printf("\nОбновленная строка: %s", s); // obnovlennaya stroka
+	printf("\nÎáíîâëåííàÿ ñòðîêà: %s", s); // obnovlennaya stroka
 }
