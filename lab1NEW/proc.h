35d563321839c2dcfe87351929504f1fe243a4d2
ProkopenkoRoman <long399@mail.ru>
2016-04-02 17:31:52 +0600

    Create proc.h
    Заголовоный файл для proc.c

---
+++ lab1NEW/proc.h
@@ -0,0 +1,13 @@
+#ifndef PROC   
+#define PROC 
+
+void input(char *s);
+void check(char *s);
+void process(char *s);
+void output(char *s);
+
+char str[261];
+char first_ch, delim;
+char *ptr[30];
+
+#endif
