88ac82338ba8ba79cca129db303203347ef6b4b6
ProkopenkoRoman <long399@mail.ru>
2016-04-02 18:38:12 +0600

    Update proc.c
    Лучшее определение ОС

--- lab1NEW/proc.c
+++ lab1NEW/proc.c
@@ -7,16 +7,16 @@ void input(char *s) {
 	int i;
 	char ch[500];
 	
-	printf("Ââåäèòå ñòðîêó, ñîîòâåòñòâóþùóþ àáñîëþòíîìó ïóòè:\n"); // vvedite stroku, sootvetstvuyushuyu absolutnomu puti	
+	printf("Введите строку, соответствующую абсолютному пути:\n"); // vvedite stroku, sootvetstvuyushuyu absolutnomu puti	
 		scanf("%s", &ch);
 	if(slen(ch) > 261) {
-	 	printf("Ïðåâûøåíèå äîïóñòèìîé äëèíû ïóòè..."); // previshenie dopustimoy dlini puti
+	 	printf("Превышение допустимой длины пути..."); // previshenie dopustimoy dlini puti
 	 	exit(1);
 	}
 	else {
 		for (i = 0; i < slen(ch); i++)
          	s[i] = ch[i]; 
-		printf("Âõîäíàÿ ñòðîêà: %s\n\n", s); // vhodnaya stroka
+		printf("Входная строка: %s\n\n", s); // vhodnaya stroka
 	} 
 }
 
@@ -41,27 +41,27 @@ void process(char *s) {
 		f=1;
 	}
 	if (f == 0) {
-		printf("Îøèáî÷íûé ïóòü...\n"); // oshibo4niy put'
+		printf("Ошибочный путь...\n"); // oshibo4niy put'
 		exit(1);
 	}
 	
-	printf("Ðàçäåëèòåëü: %c\n", delim); // razdelitel'
+	printf("Разделитель: %c\n", delim); // razdelitel'
       	
 	i = stok(s, delim, ptr);
-	printf("Êîëè÷åñòâî êàòàëîãîâ: %d\n", i - 2); // koli4estvo katalogov
+	printf("Количество каталогов: %d\n", i - 2); // koli4estvo katalogov
 	
-	printf("Êîíå÷íûé ôàéë: %s\n", ptr[i-1]); // kone4niy fayl
+	printf("Конечный файл: %s\n", ptr[i-1]); // kone4niy fayl
 	
 	s1 = slen(ptr[i-1]);
-	printf("Äëèíà êîíå÷íîãî ôàéëà: %d\n\n", s1); // dlina kone4nogo fayla
+	printf("Длина конечного файла: %d\n\n", s1); // dlina kone4nogo fayla
 						
 	for (k = 0; k < i; k++) {
 		if (isCase(ptr[k]) == 0) {
 		if (atoi(ptr[k])) {
 			j = atoi(ptr[k])+1;
-			printf("×èñëîâîé êàòàëîã, êîòîðûé íóæíî óâåëè÷èòü íà 1: <%d êàòàëîã> = %d\n", k, j); // 4islovoy katalog, kotoriy nuzhno uveli4it' na 1
+			printf("Числовой каталог, который нужно увеличить на 1: <%d каталог> = %d\n", k, j); // 4islovoy katalog, kotoriy nuzhno uveli4it' na 1
 			if (((j - 1) % 1000 == 999 || (j - 1) % 1000 == 99 || (j - 1) % 1000 == 9) && ((j - 1) % 100 == 99 || (j - 1) % 100 == 9) && ((j - 1) % 10 == 9)) {
-				printf("Íóæíî ñâäèíóòü ñòðîêó íà 1 âïðàâî, íà÷èíàÿ ñ %d êàòàëîãà\n", k); // nuzhno sdvinut' stroku na 1 vpravo, na4inaya s kataloga
+				printf("Нужно свдинуть строку на 1 вправо, начиная с %d каталога\n", k); // nuzhno sdvinut' stroku na 1 vpravo, na4inaya s kataloga
 				sdvig(s, k + 1, i, delim, ptr);
 			}
 		change(ptr[k], j);
@@ -72,5 +72,5 @@ void process(char *s) {
 }
 
 void output(char *s) {
-	printf("\nÎáíîâëåííàÿ ñòðîêà: %s", s); // obnovlennaya stroka
+	printf("\nОбновленная строка: %s", s); // obnovlennaya stroka
 }
