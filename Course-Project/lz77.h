12b4e766d922a5c25f50a296005957765532b3d4
ProkopenkoRoman <long399@mail.ru>
2016-05-13 23:54:25 +0600

    Create lz77.h
    Заголовочный файл модуля lz77.c

---
+++ Course-Project/lz77.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+#include <math.h>
+
+// прототипы вызываемых ф-ций кодирования/декодирования
+void Encode( FILE* , FILE* , int , int );
+void Decode( FILE* , FILE* );
