2c0b280f540270dc05507523ae346fe17906f05e
ProkopenkoRoman <long399@mail.ru>
2016-05-26 15:29:51 +0600

    Немного "расчесал" код

--- CourseProject/Code/main.c
+++ CourseProject/Code/main.c
@@ -8,11 +8,11 @@ int main (int argc, char ** argv)
 	char ch, op = 0; 
 	M = 512; N = 128;	// по умолчанию 
 
-	if ( argc > 3 )	//если заданы параметры в комм.строке
+	if (argc > 3)	//если заданы параметры в комм.строке
 	{
 		// проверить операцию
-		if ( strcmp(argv[1], "-c") == 0 ) op = '1';
-		else if ( strcmp(argv[1], "-d") == 0 ) op = '2';
+		if (strcmp(argv[1], "-c") == 0) op = '1';
+		else if (strcmp(argv[1], "-d") == 0) op = '2';
 		else 
 		{
 			printf("Command error!\n");
@@ -29,20 +29,20 @@ int main (int argc, char ** argv)
 	else
 	{
 		printf("Input operation!\n1 - Encode\n2 - Decode\n");
-		scanf("%c",&op );
-		if ( op == '1' ) // выбор операции кодирования
+		scanf("%c",&op);
+		if (op == '1') // выбор операции кодирования
 		{
 			printf("Input encode filename!\n");		
-			scanf("%s",&finname );
+			scanf("%s",&finname);
 			printf("Input result filename!\n");		
-			scanf("%s",&foutname );
+			scanf("%s",&foutname);
 		}
 		else if ( op == '2' ) //декодирования
 		{
 			printf("Input decode filename!\n");		
-			scanf("%s",&finname );
+			scanf("%s",&finname);
 			printf("Input result filename!\n");		
-			scanf("%s",&foutname );		
+			scanf("%s",&foutname);		
 		}
 		else
 		{
@@ -51,45 +51,45 @@ int main (int argc, char ** argv)
 		}	
 	}	
 	// при воборе кодирования
-	if ( op == '1' )
+	if (op == '1')
 	{
 		printf("Size Dictionary : %d\n", M);
 		printf("Size Buffer : %d\n", N);	
 		// приглашение изменить размеры словаря и буфера
 		printf("Change Size? (Y) \n", M);
 		fflush(stdin);
-		scanf("%c", &ch );
+		scanf("%c", &ch);
 		
-		if ( ch == 'Y' || ch == 'y' )
+		if (ch == 'Y' || ch == 'y')
 		{
 			printf("Input Size Dictionary !\n");
-			scanf("%d", &M );
+			scanf("%d", &M);
 			printf("Input Size Buffer !\n");
-			scanf("%d", &N );	
+			scanf("%d", &N);	
 		}
 	}
 	
 	//открытие исходного файла
 	fin = fopen(&finname, "rb");
-	if ( fin == NULL ) 
+	if (fin == NULL) 
 	{
 		printf("Error: open read file!");
 		return 3;
 	}
 	// открытие файла для записи результат
 	fout = fopen(&foutname, "wb");
-	if ( fout == NULL ) 
+	if (fout == NULL) 
 	{
 		printf("Error: open write file!");
 		fclose(fin);
 		return 3;
 	}
 
-	if ( op == '1' ) {	// вызов ф-ции кодирования
-		Encode( fin, fout, M, N );
+	if (op == '1') {	// вызов ф-ции кодирования
+		Encode(fin, fout, M, N);
 	}
-	else  {	//декодирования
-		Decode( fin, fout );
+	else {	//декодирования
+		Decode(fin, fout);
 	}
 	
 	// закрыть файлы
