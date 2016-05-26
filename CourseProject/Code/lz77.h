15ad8ecc2791b94f6a5986f135cb8d362ed0e8b6
ProkopenkoRoman <long399@mail.ru>
2016-05-26 15:29:27 +0600

    Немного "расчесал" код

--- CourseProject/Code/lz77.h
+++ CourseProject/Code/lz77.h
@@ -3,6 +3,13 @@
 #include <string.h>
 #include <math.h>
 
+//структура кода
+struct Code {
+	int num;	//индекс в словаре
+	int len;	//длина найденной подстроки
+	char c;		//символ стоящий за подстрокой из буфера
+};
+
 // прототипы вызываемых ф-ций кодирования/декодирования
-void Encode( FILE* , FILE* , int , int );
-void Decode( FILE* , FILE* );
+void Encode(FILE* , FILE* , int , int);
+void Decode(FILE* , FILE*);
